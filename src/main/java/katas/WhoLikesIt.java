e910805be4f4812dd591ef3d531095446878d648
chloefaurie <chloe.faurie@gmail.com>
2025-12-11 16:23:48 +0100

    add switch case

--- src/main/java/katas/WhoLikesIt.java
+++ src/main/java/katas/WhoLikesIt.java
@@ -26,3 +26,21 @@ class Solution {
         return "";
     }
 }
+
+class Solution1 {
+    public static String whoLikesIt(String... names) {
+        int length = names.length;
+        switch(length){
+            case 0:
+                return "no one likes this";
+            case 1:
+                return String.valueOf(names[0])+" likes this";
+            case 2:
+                return String.valueOf(names[0])+" and "+String.valueOf(names[1])+" like this";
+            case 3:
+                return String.valueOf(names[0])+", "+String.valueOf(names[1])+ " and "+String.valueOf(names[2])+" like this";
+            default:
+                return String.valueOf(names[0])+", "+String.valueOf(names[1])+ " and "+ String.valueOf(names.length-2)+" others like this";
+        }
+    }
+}
