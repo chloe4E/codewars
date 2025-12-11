fe6f9a378a0d1b1376152a1bea9c27ca250f5d82
chloefaurie <chloe.faurie@gmail.com>
2025-12-11 14:12:33 +0100

    add template

---
+++ src/main/java/katas/TEMPLATE.java
@@ -0,0 +1,15 @@
+package katas;
+
+/**
+ * [Kata Name]
+ * Difficulty: [kyu level]
+ * URL: https://www.codewars.com/kata/...
+ *
+ * Description:
+ * [Brief description]
+ */
+public class YourKataName {
+    public static ReturnType method(ParameterType param) {
+        // Your solution
+    }
+}
