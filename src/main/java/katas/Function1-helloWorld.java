1d44a57d53c5af4e4553964b21d319c44fed7cde
chloefaurie <chloe.faurie@gmail.com>
2025-12-19 10:17:58 +0100

    hello world

---
+++ src/main/java/katas/Function1-helloWorld.java
@@ -0,0 +1,16 @@
+package katas;
+
+/**
+ * [Function 1 - hello world]
+ * Difficulty: [8 kyu]
+ * URL: https://www.codewars.com/kata/523b4ff7adca849afe000035/train/java
+ * <p>
+ * Description:
+ * Make a simple function called greet that returns the most-famous "hello world!".
+ */
+
+public class HelloWorld {
+	public static String greet() {
+		return "hello world!";
+	}
+}
