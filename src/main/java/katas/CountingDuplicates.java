fff6baa31376c3710aa52b6f503a8af342ff31aa
chloefaurie <chloe.faurie@gmail.com>
2025-12-23 15:00:08 +0100

    add desc for CountingDuplicates

--- src/main/java/katas/CountingDuplicates.java
+++ src/main/java/katas/CountingDuplicates.java
@@ -6,7 +6,8 @@ package katas;
  * URL: https://www.codewars.com/kata/54bf1c2cd5b56cc47f0007a1/train/java
  * <p>
  * Description:
- * [Brief description]
+ * Count the number of Duplicates
+ * Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
  */
 
 import java.util.*;
