b11e0e3cebf104c1057bc4b8cf14d05aa4a0d44e
chloefaurie <chloe.faurie@gmail.com>
2026-02-17 14:39:57 +0100

    encrypt this

---
+++ src/main/com/codewars/katas/EncryptThis.java
@@ -0,0 +1,47 @@
+package com.codewars.katas;
+
+/*
+ * [encryptThis]
+ * Difficulty: [6 kyu]
+ * URL: https://www.codewars.com/kata/5848565e273af816fb000449/train/java
+ * <p>
+ * Description:
+ * [Brief description]
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+
+public class EncryptThis {
+	public static String encryptThis(String text) {
+
+		if (Objects.equals(text, "")) {
+			return "";
+		}
+		// Implement me! :)
+		List<String> myList = new ArrayList<>(Arrays.asList(text.split(" ")));
+		List<String> result = new ArrayList<>();
+
+		for (String word : myList) {
+			StringBuilder sb = new StringBuilder(word);
+			int first = word.charAt(0);
+			if (word.length() < 2) {
+				sb.delete(0, 1);
+				sb.insert(0, first);
+			} else {
+				String second = String.valueOf(word.charAt(1));
+				String last = String.valueOf(word.charAt(word.length() - 1));
+				sb.delete(0, 1);
+				sb.insert(0, first);
+				sb.replace(String.valueOf(first).length(), String.valueOf(first).length() + 1, last);
+				sb.setLength(sb.length() - 1);
+				sb.append(second);
+			}
+			result.add(sb.toString());
+		}
+		return String.join(" ", result);
+	}
+}
