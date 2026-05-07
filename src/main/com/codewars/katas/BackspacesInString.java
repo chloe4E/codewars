560e52c21b83164b280435cb44b1867f2b3c0188
chloefaurie <chloe.faurie@gmail.com>
2026-05-07 10:05:35 +0200

    BackspacesInString

--- src/main/com/codewars/katas/BackspacesInString.java
+++ src/main/com/codewars/katas/BackspacesInString.java
@@ -3,6 +3,8 @@ package com.codewars.katas;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * [BackspacesInString]
  * Difficulty: [6 kyu]
@@ -18,20 +20,29 @@ public class BackspacesInString {
 			ArrayList<String> myArr = new ArrayList(Arrays.asList(s.split("")));
 
 			if (! myArr.contains("#")){
-				return myArr.toArray().toString();
+				return s;
 			}
 
 			for (int i=0; i < myArr.size(); i++) {
-				if(myArr.get(i) == "#" && i > 0){
+				if (myArr.get(i).equals("#")  && i == 0){
+					myArr.remove(i);
+					i = i-1;
+				} else if(myArr.get(i).equals("#")  && i > 0){
 					// not allowed:
 					//myArr.removeRange(i-1,i+1);
 					myArr.subList(i-1,i+1).clear();
 					i = i-2;
 				}
 			}
-			return myArr.toArray().toString();
-
-
+			return String.join("", myArr);
 
 		}
-	}
+
+		public static void main(String[] args) {
+				final BackspacesInString bis = new BackspacesInString();
+				assertEquals("abc", bis.cleanString("abc"));
+				assertEquals("ac", bis.cleanString("abc#d##c"));
+				assertEquals("", bis.cleanString("#########"));
+				assertEquals("", bis.cleanString("abc####d##c#"));
+			}
+		}
