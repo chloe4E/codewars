beec9537c4293944d93085b76c0620ab04a2d165
chloefaurie <chloe.faurie@gmail.com>
2026-06-18 16:41:34 +0200

    add kata

--- src/main/com/codewars/katas/LongestSubstringKUnique.java
+++ src/main/com/codewars/katas/LongestSubstringKUnique.java
@@ -17,7 +17,6 @@ public class LongestSubstringKUnique {
       return s.length();
     }
     // Your solution here
-    int countUnique = 0;
     int currentLen = 0;
     int maxLen = 0;
     List<Character> letters = new ArrayList<>();
@@ -26,25 +25,19 @@ public class LongestSubstringKUnique {
       if (s.substring(start, finish).length() < uniqueNumberOfCharacters) {
         break;
       }
-      System.out.println(s.substring(start, finish));
       for (char c : s.substring(start, finish).toCharArray()) {
-        System.out.println("starting for: " + c);
         if (!letters.contains(c)) {
-          System.out.println("letter does not contain: " + c);
           if (letters.size() < uniqueNumberOfCharacters) {
             letters.add(c);
             currentLen = currentLen + 1;
           } else {
             maxLen = Math.max(currentLen, maxLen);
-            System.out.println("maxLen: " + maxLen);
             currentLen = 0;
             letters.clear();
             break;
           }
         } else if (letters.contains(c)) {
-          System.out.println("letter contains: " + c);
           currentLen = currentLen + 1;
-          System.out.println("currentLen: " + currentLen);
         }
       }
     }
